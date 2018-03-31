52428e87db730ad6c991c5270e271687300763ee
metajake <jaynoco@gmail.com>
2018-03-31 18:50:38 -0500

    Save Data Between Scenes

---
+++ Visual Novel 02/Assets/Scripts/AdjustControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustControl : MonoBehaviour {
+
+	private void OnGUI()
+	{
+        if (GUI.Button(new Rect(10, 100, 100, 30), "Health Up")){
+            GameControl.control.health += 10;
+        }
+        if (GUI.Button(new Rect(10, 140, 100, 30), "Health Down"))
+        {
+            GameControl.control.health -= 10;
+        }
+        if (GUI.Button(new Rect(10, 180, 100, 30), "Experience Up"))
+        {
+            GameControl.control.experience += 100;
+        }
+        if (GUI.Button(new Rect(10, 220, 100, 30), "Experience Down"))
+        {
+            GameControl.control.experience -= 100;
+        }
+	}
+}
---
+++ Visual Novel 02/Assets/Scripts/GameControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+
+    public float health;
+    public float experience;
+
+	// Use this for initialization
+	void Awake () {
+        if(control == null){
+	        DontDestroyOnLoad(gameObject);
+            control = this;
+        }else if(control != this){
+            Destroy(gameObject);
+        }
+	}
+
+	private void OnGUI()
+	{
+        GUI.Label(new Rect(10, 10, 100, 30), "Health: " + health);
+        GUI.Label(new Rect(10, 40, 100, 50), "Experience: " + experience);
+	}
+}
